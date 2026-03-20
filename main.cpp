1ae3bcf8010951e0c0b088584767515560cde041
Oleksandr Krasichkov <alexkrasichkov@gmail.com>
2026-03-20 22:25:24 +0100

    calculates the biggest distance between 2 cities

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <cmath>
 using namespace std;
 struct City{
 	string name;
@@ -14,8 +15,6 @@ int main()
 	vector<City> cities;
 	City c;
 	ifstream file("cities10k.txt");
-	string name;
-	double lat, lon;
 	if(file.is_open())
 	{
 		while(file>>c.name>>c.lat>>c.lon)
@@ -25,9 +24,26 @@ int main()
 		
 	}
 	file.close();
+	double lat = 0,lon = 0, maxv = 0;
+	string maxcity1, maxcity2;
+	for (int i = 0; i < cities.size() - 1; ++i)
+	{
+		for(int j = i+1; j < cities.size(); ++j)
+		{
+			double lats = cities[i].lat - cities[j].lat;
+			double lons = cities[i].lon - cities[j].lon;
+			double distance = sqrt(lats*lats + lons*lons);
+			if(maxv < distance)
+			{
+				maxv = distance;
+				maxcity1 = cities[i].name;
+				maxcity2 = cities[j].name;
+			}
+		}
+	}
+cout<<"\nthe biggest dist:"<<endl;
+cout<<maxcity1<<" and "<<maxcity2<<" with distance = "<<maxv<<endl;
 
 
-
-
-
+return 0;
 }
